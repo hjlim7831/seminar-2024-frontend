441ac2d01d3eb330f59715e9065f48c58520ee61
hjlim7831 <103012931+hjlim7831@users.noreply.github.com>
2024-09-12 20:54:38 +0900

    feat: moveMapIn2048Rule 추가 (#1)
    * feat: moveMapIn2048Rule 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,16 @@
 import './App.css';
 
-import { useState } from 'react';
-
-import viteLogo from '/vite.svg';
-
-import reactLogo from './assets/react.svg';
+import { type Map2048, moveMapIn2048Rule } from './utils/moveMapIn2048Rule';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const map: Map2048 = [
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  const res = moveMapIn2048Rule(map, 'up');
+
+  return <>{res}</>;
 }
 
 export default App;
