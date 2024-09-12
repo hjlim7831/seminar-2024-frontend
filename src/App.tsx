7dae23d4a5bce0d79d9ee4ccb9ff4ac4cfce61b9
임혜진 <hjlim7831@gmail.com>
2024-09-12 20:44:59 +0900

    feat: moveMapIn2048Rule 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
 import './App.css';
 
-import { type Map2048, moveMapIn2048Rule } from './utils/moveMapIn2048Rule';
-
 function App() {
-  const map: Map2048 = [
-    [0, 0, 0],
-    [0, 0, 0],
-  ];
-
-  const res = moveMapIn2048Rule(map, 'up');
-
-  return <>{res}</>;
+  return <></>;
 }
 
 export default App;
