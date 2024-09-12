7b8935f4082ddd490081caf9f6f17b5bc355f1de
임혜진 <hjlim7831@gmail.com>
2024-09-12 20:53:32 +0900

    docs: yarn check all

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css';
 
+import { type Map2048, moveMapIn2048Rule } from './utils/moveMapIn2048Rule';
+
 function App() {
-  return <></>;
+  const map: Map2048 = [
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+
+  const res = moveMapIn2048Rule(map, 'up');
+
+  return <>{res}</>;
 }
 
 export default App;
